53f4284286d0ca725bc6cdedc9ca96a190bebbcf
Madan <madanstha.124@gmail.com>
2020-09-08 19:27:20 +0545

    Models added

--- Job_Portal/JobApp/models.py
+++ Job_Portal/JobApp/models.py
@@ -1,3 +1,61 @@
 from django.db import models
 
-# Create your models here.
+
+class JobSeeker(models.Model):
+    GENDER_CHOICES = (
+        ('male', 'Male'),
+        ('female', 'Female'))
+    name = models.CharField(max_length=50, blank=False)
+    email = models.EmailField()
+    date_of_birth = models.DateTimeField(null=True)
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES, null=False)
+    address = models.CharField(max_length=100, null=True)
+    contact = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Company(models.Model):
+    TYPE_CHOICES = (
+        ('Airlines', 'Airlines'),
+        ('Automotive Sales, Support & Service', 'Automotive Sales, Support & Service'),
+        ('Banks', 'Banks'),
+        ('Call Center', 'Call Center'),
+        ('Consumer Products', 'Consumer Products'),
+        ('Designing / Printing', 'Designing / Printing'),
+        ('E-business', 'E-business'),
+        ('Education', 'Education'),
+        ('Event Management', 'Event Management'),
+        ('Finance', 'Finance'),
+        ('Governmental Company', 'Governmental Company'),
+        ('Health', 'Health'),
+        ('Hotels', 'Hotels'),
+        ('Insurance', 'Insurance'),
+        ('Information Technology', 'Information Technology'),
+        ('Import/Export', 'Import/Export'),
+        ('ISP', 'ISP'),
+        ('Media', 'Media'),
+        ('NGO', 'NGO'),
+        ('Real Estate', 'Real Estate'),
+        ('Travel Agency', 'Travel Agency')
+    )
+    name = models.CharField(max_length=50, null=False)
+    type = models.CharField(max_length=50, choices=TYPE_CHOICES, null=False)
+    address = models.CharField(max_length=50, null=False)
+    contact = models.IntegerField(null=True)
+    email = models.EmailField(null=True)
+    description = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Job(models.Model):
+    title = models.CharField(max_length=50, null=False)
+    company_name = models.ForeignKey(Company, on_delete=models.CASCADE, null=False)
+    post_date = models.DateTimeField(auto_now_add=True)
+    last_date = models.DateTimeField(null=False)
+
+    def __str__(self):
+        return self.title
